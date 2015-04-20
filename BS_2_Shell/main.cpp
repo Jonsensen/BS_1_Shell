b59d36d4a0ce580c9f139cf4d846a3d26caa0085
Jonathan Fertig <Jonsen@jonfemac.local>
2015-04-20 20:21:53 +0200

    Initial Commit

---
+++ BS_2_Shell/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BS_2_Shell
+//
+//  Created by Jonathan Fertig on 20.04.15.
+//  Copyright (c) 2015 Jonathan Fertig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
