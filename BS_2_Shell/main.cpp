b95e62aaffdfe16821f882b3505639b5f40fb0f2
Jonathan Fertig <Jonsen@studpool-wlan-75-203.fs.fbi.h-da.de>
2015-04-27 14:13:48 +0200

    Gerüst erweitert. Daten verloren in readLine

--- BS_2_Shell/main.cpp
+++ BS_2_Shell/main.cpp
@@ -7,13 +7,46 @@
 #include <stdio.h>				// printf(), ...
 #include <time.h>				// time(), ...
 #include <string.h>				// strtok()
+#include <iostream>
+#include <sstream>
+#include <vector>
 #define MAXLINE 100
 #define MOD "exit with CTR C"
 
 
+
+
 int read_command(char *command, char *parameters[]) { // prompt for user input and read a command line
     fprintf(stdout, "$ ");
-        
+    
+    char wholeLine[MAXLINE];
+    
+    std::cin.getline(wholeLine,MAXLINE);
+  
+    std::vector<char*>args;
+    
+    //char* prog= strtok(command, " ");
+    command=strtok(wholeLine," "); // Hier anders !!!
+    char* tmp= command;
+    while(tmp!=NULL){
+        args.push_back(tmp);
+        tmp=strtok(NULL, " ");
+    }
+    // argv = parameters
+    
+    parameters=new char*[args.size()+1];
+    for (int i=0; i<args.size(); i++) {
+        parameters[i]=args[i];
+       }
+    
+     parameters[args.size()]=NULL;
+   
+  
+    /*
+    std::string tmpStr=command;
+    std::stringstream tmpStream(tmpStr);
+ */
+    
     
     
     return 0;
@@ -25,17 +58,28 @@ int main(int argc, char *argv[]) {
     char command[MAXLINE];
     char *parameters[MAXLINE];
     int noParams;
-    
     while (1) {
+        
         noParams = read_command(command, parameters); // read user input
-        return 0;
+        std::cout<<parameters<<std::endl;
+        std::cout<<command<<std::endl;
         if ((childPid = fork()) == -1) { // create process
             fprintf(stderr, "can't fork!\n");
             exit(3);
-        } else if (childPid == 0) { // child process
-            //execvp(command, parameters); // executes command
+        }
+        
+        else if (childPid == 0) { // child process
+            std::cout<<"Im Kind"<<std::endl;
+            std::cout<<*parameters<<std::endl;
+            std::cout<<command<<std::endl;
+            
+           //status= execvp(parameters[0], parameters); // executes command
+            status=execvp(command,parameters);
+        
             exit(3);
-        } else { // father
+        }
+        
+        else { // father
             waitpid(childPid, &status, WUNTRACED | WCONTINUED);
         }
     }
